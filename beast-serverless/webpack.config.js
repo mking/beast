b73d64527ab00c5e6d41d088ae418f28640801e6
Matthew King <mking@users.noreply.github.com>
2018-01-14 03:40:12 -0800

    without webpack

--- beast-serverless/webpack.config.js
+++
@@ -1,8 +0,0 @@
-const nodeExternals = require('webpack-node-externals');
-const slsw = require('serverless-webpack');
-
-module.exports = {
-  entry: slsw.lib.entries,
-  target: 'node',
-  externals: [nodeExternals()]
-};
