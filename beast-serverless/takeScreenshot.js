fda706844b3dc7b7ed07ee50ab071f91bf0833de
Matthew King <mking@users.noreply.github.com>
2018-01-14 17:51:04 -0800

    stop work on serverless

--- beast-serverless/takeScreenshot.js
+++ beast-serverless/takeScreenshot.js
@@ -8,8 +8,9 @@ const path = require('path');
 const puppeteer = require('puppeteer');
 const tar = require('tar');
 
-const CHROME_TMP = '/tmp';
+const TMP = '/tmp';
 const CHROME_KEY = 'headless-chromium.tar.gz';
+const SCREENSHOT_KEY = 'screenshot.png';
 
 function downloadBrowser() {
   if (process.env.IS_LOCAL === 'true') {
@@ -25,12 +26,12 @@ function downloadBrowser() {
     })
     .promise()
     .then(data => {
-      return pify(fs.writeFile)(path.join(CHROME_TMP, CHROME_KEY), data.Body);
+      return pify(fs.writeFile)(path.join(TMP, CHROME_KEY), data.Body);
     })
     .then(() => {
       return tar.x({
-        file: path.join(CHROME_TMP, 'headless-chromium.tar.gz'),
-        cwd: CHROME_TMP
+        file: path.join(TMP, 'headless-chromium.tar.gz'),
+        cwd: TMP
       });
     })
     .then(() => console.log('Downloaded chrome'));
@@ -44,7 +45,7 @@ function startBrowser() {
       process.env.IS_LOCAL === 'true'
         ? {}
         : {
-            chromePath: path.join(CHROME_TMP, 'headless-chromium')
+            chromePath: path.join(TMP, 'headless-chromium')
           }
     )
   ).then(browser => {
@@ -67,7 +68,7 @@ function connectBrowserWithFallback() {
   console.log('Trying to connect to chrome...');
   return connectBrowser()
     .catch(error => {
-      if (error.code === 'ECONNREFUSED') {
+      if (error.code === 'ECONNREFUSED' || error.code === 'ECONNRESET') {
         return downloadBrowser()
           .then(() => startBrowser())
           .then(() => connectBrowser());
@@ -86,34 +87,51 @@ function testBrowser(browser) {
     .then(page => {
       console.log('Going to the address...');
       return page
-        .goto('https://news.ycombinator.com')
-        .then(() => {
-          console.log('Taking screenshot...');
-          return page.screenshot();
-        })
-        .then(buffer => {
-          const s3 = new AWS.S3();
-          return s3
-            .putObject({
-              Bucket: 'beast-lambda',
-              Key: 'screenshot.png',
-              Body: buffer,
-              ACL: 'public-read',
-              ContentType: 'image/png'
-            })
-            .promise();
-        });
+        .goto('https://s3-us-west-2.amazonaws.com/beast-lambda/index.html')
+        .then(() =>
+          page.evaluate(() => document.querySelector('h1').textContent)
+        )
+        .then(text => console.log(text));
+      // .then(() => {
+      //   console.log('Taking screenshot...');
+      //   return page.screenshot({
+      //     path: path.join(TMP, SCREENSHOT_KEY)
+      //   });
+      // })
+      // .then(() => {
+      //   return pify(fs.readFile)(path.join(TMP, SCREENSHOT_KEY));
+      // })
+      // .then(data => {
+      //   const s3 = new AWS.S3();
+      //   return s3
+      //     .putObject({
+      //       Bucket: 'beast-lambda',
+      //       Key: SCREENSHOT_KEY,
+      //       Body: data,
+      //       ACL: 'public-read',
+      //       ContentType: 'image/png'
+      //     })
+      //     .promise();
+      // });
     })
     .then(() => browser.close());
 }
 
 exports.handler = function(event, context, callback) {
-  console.log('Starting lambda...');
-  connectBrowserWithFallback()
-    .then(browser => testBrowser(browser))
-    .then(() => {
-      console.log('Finished lambda');
+  fetch('https://s3-us-west-2.amazonaws.com/beast-lambda/index.html')
+    .then(response => response.text())
+    .then(text => {
+      console.log(text);
       callback();
     })
     .catch(error => console.error(error));
+
+  // console.log('Starting lambda...');
+  // connectBrowserWithFallback()
+  //   .then(browser => testBrowser(browser))
+  //   .then(() => {
+  //     console.log('Finished lambda');
+  //     callback();
+  //   })
+  // .catch(error => console.error(error));
 };
